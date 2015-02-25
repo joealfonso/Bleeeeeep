777c2c85acb960ff96de82f759ba89a4f2620e67
switchmotiv <j.alfonso84@gmail.com>
2015-02-24 18:56:00 -0600

    Text input
    Added functionality for user to input Youtube URL and parse the video
    ID.

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,7 @@
 
 // 2. This code loads the IFrame Player API code asynchronously.
 var tag = document.createElement('script');
+var userInput = $('#input-8').val();
 
 tag.src = "https://www.youtube.com/iframe_api";
 var firstScriptTag = document.getElementsByTagName('script')[0];
@@ -41,38 +42,6 @@ function stopVideo() {
 };
 
 
-//<!--Script for input field-->
 
-(function() {
-	// trim polyfill : https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/Trim
-	if (!String.prototype.trim) {
-		(function() {
-			// Make sure we trim BOM and NBSP
-			var rtrim = /^[\s\uFEFF\xA0]+|[\s\uFEFF\xA0]+$/g;
-			String.prototype.trim = function() {
-				return this.replace(rtrim, '');
-			};
-		})();
-	}
 
-	[].slice.call( document.querySelectorAll( 'input.input__field' ) ).forEach( function( inputEl ) {
-		// in case the input is already filled..
-		if( inputEl.value.trim() !== '' ) {
-			classie.add( inputEl.parentNode, 'input--filled' );
-		}
 
-		// events:
-		inputEl.addEventListener( 'focus', onInputFocus );
-		inputEl.addEventListener( 'blur', onInputBlur );
-	} );
-
-	function onInputFocus( ev ) {
-		classie.add( ev.target.parentNode, 'input--filled' );
-	};
-
-	function onInputBlur( ev ) {
-		if( ev.target.value.trim() === '' ) {
-			classie.remove( ev.target.parentNode, 'input--filled' );
-		}
-	}
-});
